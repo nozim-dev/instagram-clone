cf234e430b969c6a5a7a0ed16c4483c3240db88d
nozim-dev <jusfotacouenglish@gmail.com>
2023-07-29 11:53:45 +0500

    secured file

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import React from "react";
 import SignIn from "./Page/SignIn/SignIn";
 import SignUp from "./Page/SignUp/SignUp";
 const App = () => {
+  function RightClick(event) {
+    event.preventDefault(); // Prevent the default right-click menu behavior
+  }
   return (
-    <div className="App">
+    <div className="App" onContextMenu={RightClick}>
       <SignIn />
       <SignUp />
     </div>
--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -3,8 +3,38 @@ import Aside from "../Components/Aside/Aside";
 import { Outlet } from "react-router-dom";
 
 const HomeLayout = () => {
+  function RightClick(event) {
+    event.preventDefault(); // Prevent the default right-click menu behavior
+  }
+
+  document.onkeydown = function (e) {
+    // disable F12 key
+    if (e.keyCode == 123) {
+      return false;
+    }
+
+    // disable I key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 73) {
+      return false;
+    }
+
+    // disable C key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 67) {
+      return false;
+    }
+
+    // disable J key
+    if (e.ctrlKey && e.shiftKey && e.keyCode == 74) {
+      return false;
+    }
+
+    // disable U key
+    if (e.ctrlKey && e.keyCode == 85) {
+      return false;
+    }
+  };
   return (
-    <div className="Container">
+    <div onContextMenu={RightClick} className="Container">
       <Aside />
       <Outlet />
     </div>
